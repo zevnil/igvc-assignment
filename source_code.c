234affd8e101694cf8dafd5f3c31e470818605e6
zevnil <58893304+zevnil@users.noreply.github.com>
2021-11-26 14:19:06 +0530

    Add files via upload

---
+++ source_code.c
@@ -0,0 +1,77 @@
+//Submitted by: Jyotsana Srivastava - IIT2019174
+#include <GL/glut.h>
+#include <math.h>
+float leg_width = 2;
+float table_leg_height = 10;
+float table_top_size = 15;
+float platter_width = 4;
+float platter_size = 10;
+float circle_r = 2;
+float chair_distance = 5;
+float l = 5;
+
+static void drawDisc(float cx, float cy, float radius) {
+    glBegin(GL_POLYGON);
+    double PI = 3.14;
+    for (int i = 0; i <= 360; i++) {
+        double angle = 2 * PI * i / 360;
+        double x = cos(angle) * radius;
+        double y = sin(angle) * radius;
+        glVertex2d(cx + x, cy + y);
+    }
+    glEnd();
+}
+void renderScene() {
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50, 50, -50, 50, -1, 1);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //table
+    glColor3f(0.5f, 0.35f, 0.05f);//brown
+    glRectf(0, 0, leg_width, table_leg_height);
+    glRectf(0, table_leg_height, table_top_size, table_leg_height + leg_width);
+    glRectf(table_top_size, table_leg_height + leg_width, table_top_size + leg_width, 0);
+    float table_center[2] = { table_top_size / 2, table_leg_height + leg_width };
+
+
+    //balls
+    float platter_center[2] = { table_center[0],table_center[1] + platter_width };
+    glColor3f(0.8f, 0.47f, 34 / 255);// ochre
+    drawDisc(platter_center[0] - circle_r / 2, platter_center[1] + circle_r / 2, circle_r);
+    glColor3f(1, 0, 0);// red
+    drawDisc(platter_center[0] + circle_r / 2, platter_center[1] + circle_r / 2, circle_r);
+    //platter
+    glColor3f(0, 1, 0);//green
+    glRectf(table_center[0] - (platter_size / 2), table_center[1], table_center[0] + (platter_size / 2), table_center[1] + platter_width);
+    //chair leg 1
+    float table_bottom_right[2] = { table_top_size + leg_width, 0 };
+    float chair_br[2] = { table_bottom_right[0] + chair_distance, 0 };
+    glColor3f(0, 0, 1);//blue
+    glRectf(chair_br[0], 0, chair_br[0] + leg_width, l);
+    //chair leg 2
+    float table_bottom_l[2] = { table_top_size + leg_width, 0 };
+    float chair_bl[2] = { table_bottom_l[0] + 2*chair_distance, 0 };
+    glColor3f(0, 0, 1);//blue
+    glRectf(chair_bl[0], 0, chair_bl[0] + leg_width, 5*l);
+    //chair seat
+    float table_bottom_m[2] = { table_top_size + leg_width, 0 };
+    float chair_m[2] = { table_bottom_m[0] + chair_distance, 0 };
+    glColor3f(0, 0, 1);//blue
+    glRectf(chair_m[0], l, chair_m[0] + chair_distance, l/2);
+    glFlush();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutCreateWindow("IIT2019174");
+    glutDisplayFunc(renderScene);
+    glutMainLoop();
+    return 0;
+}
